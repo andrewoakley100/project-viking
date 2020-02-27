899189f004aaeada41cddacccf5f8db859859dd5
Nathan <nathan.wuiske@griffithuni.edu.au>
2020-02-27 16:36:10 +1000

    add actions re auth

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -24,7 +24,67 @@ const getters = {
   }
 };
 
+const actions = {
+  [LOGIN](context, credentials) {
+    return new Promise(resolve => {
+      ApiService.post("users/login", { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+          resolve(data);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    });
+  },
+  [LOGOUT](context) {
+    context.commit(PURGE_AUTH);
+  },
+  [REGISTER](context, credentials) {
+    return new Promise((resolve, reject) => {
+      ApiService.post("users", { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+          resolve(data);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+          reject(response);
+        });
+    });
+  },
+  [CHECK_AUTH](context) {
+    if (JwtService.getToken()) {
+      ApiService.setHeader();
+      ApiService.get("user")
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    } else {
+      context.commit(PURGE_AUTH);
+    }
+  },
+  [UPDATE_USER](context, payload) {
+    const { email, username, password, image, bio } = payload;
+    const user = {
+      email,
+      username,
+      bio,
+      image
+    };
+    if (password) {
+      user.password = password;
+    }
 
+    return ApiService.put("user", user).then(({ data }) => {
+      context.commit(SET_AUTH, data.user);
+      return data;
+    });
+  }
+};
 
 export default {
   state,
