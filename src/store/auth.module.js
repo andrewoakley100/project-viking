3c52e609ce71a6bd09c7d70b2eb3a7e8ba509e23
Nathan <nathan.wuiske@griffithuni.edu.au>
2020-02-27 16:35:14 +1000

    auth scaffolding with state

---
+++ src/store/auth.module.js
@@ -0,0 +1,27 @@
+import ApiService from "@/common/api.service";
+import JwtService from "@/common/jwt.service";
+import {
+  LOGIN,
+  LOGOUT,
+  REGISTER,
+  CHECK_AUTH,
+  UPDATE_USER
+} from "./actions.type";
+import { SET_AUTH, PURGE_AUTH, SET_ERROR } from "./mutations.type";
+
+const state = {
+  errors: null,
+  user: {},
+  isAuthenticated: !!JwtService.getToken()
+};
+
+
+
+
+
+export default {
+  state,
+  actions,
+  mutations,
+  getters
+};
