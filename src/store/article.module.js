ce39eb16778cb45401dd685a85ed5157ed7d0741
Nathan <nathan.wuiske@griffithuni.edu.au>
2020-02-27 16:38:26 +1000

    article actions C|R|U|D

--- src/store/article.module.js
+++ src/store/article.module.js
@@ -38,7 +38,59 @@ const initialState = {
   comments: []
 };
 
-
+export const actions = {
+  async [FETCH_ARTICLE](context, articleSlug, prevArticle) {
+    // avoid extronuous network call if article exists
+    if (prevArticle !== undefined) {
+      return context.commit(SET_ARTICLE, prevArticle);
+    }
+    const { data } = await ArticlesService.get(articleSlug);
+    context.commit(SET_ARTICLE, data.article);
+    return data;
+  },
+  async [FETCH_COMMENTS](context, articleSlug) {
+    const { data } = await CommentsService.get(articleSlug);
+    context.commit(SET_COMMENTS, data.comments);
+    return data.comments;
+  },
+  async [COMMENT_CREATE](context, payload) {
+    await CommentsService.post(payload.slug, payload.comment);
+    context.dispatch(FETCH_COMMENTS, payload.slug);
+  },
+  async [COMMENT_DESTROY](context, payload) {
+    await CommentsService.destroy(payload.slug, payload.commentId);
+    context.dispatch(FETCH_COMMENTS, payload.slug);
+  },
+  async [FAVORITE_ADD](context, slug) {
+    const { data } = await FavoriteService.add(slug);
+    context.commit(UPDATE_ARTICLE_IN_LIST, data.article, { root: true });
+    context.commit(SET_ARTICLE, data.article);
+  },
+  async [FAVORITE_REMOVE](context, slug) {
+    const { data } = await FavoriteService.remove(slug);
+    // Update list as well. This allows us to favorite an article in the Home view.
+    context.commit(UPDATE_ARTICLE_IN_LIST, data.article, { root: true });
+    context.commit(SET_ARTICLE, data.article);
+  },
+  [ARTICLE_PUBLISH]({ state }) {
+    return ArticlesService.create(state.article);
+  },
+  [ARTICLE_DELETE](context, slug) {
+    return ArticlesService.destroy(slug);
+  },
+  [ARTICLE_EDIT]({ state }) {
+    return ArticlesService.update(state.article.slug, state.article);
+  },
+  [ARTICLE_EDIT_ADD_TAG](context, tag) {
+    context.commit(TAG_ADD, tag);
+  },
+  [ARTICLE_EDIT_REMOVE_TAG](context, tag) {
+    context.commit(TAG_REMOVE, tag);
+  },
+  [ARTICLE_RESET_STATE]({ commit }) {
+    commit(RESET_STATE);
+  }
+};
 
 export default {
   state,
